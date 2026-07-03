cfe5313601e61c94f49a7ea80bc299e596ecd7d4
Redi38 <alecspetrow88@gmail.com>
2026-07-03 20:02:40 +0000

    added logo on webadmin

--- admin/app.py
+++ admin/app.py
@@ -4,6 +4,7 @@ import aiohttp
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from sqladmin import Admin, ModelView
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
@@ -28,6 +29,13 @@ app = FastAPI(
 )
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get('/favicon.ico', include_in_schema=False)
+async def favicon():
+    favicon_path = "static/favicon.ico"
+    if os.path.exists(favicon_path):
+        return FileResponse(favicon_path)
+    return FileResponse("static/favicon.ico")
 
 templates = Jinja2Templates(directory="templates")
 
@@ -182,7 +190,7 @@ async def admin_dashboard(request: Request):
 
     return templates.TemplateResponse(
         request=request,
-        name="layout.html",
+        name="sqladmin/index.html",
         context={"request": request, "stats": stats}
     )
 
